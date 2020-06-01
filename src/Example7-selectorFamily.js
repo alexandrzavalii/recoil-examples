86dffaf472bb9692d5b2f0f9740a94bf2d595d53
Alex <alex.zavalii@workday.com>
2020-06-01 10:01:43 +0100

    remove unneccessary imports

--- src/Example7-selectorFamily.js
+++ src/Example7-selectorFamily.js
@@ -1,42 +1,37 @@
-import React from 'react';
-import {
-    atom,
-    atomFamily,
-    useRecoilState,
-    selectorFamily,
-    useRecoilValue,
-} from 'jared-recoil';
+import React from "react";
+import { atom, atomFamily, selectorFamily, useRecoilValue } from "jared-recoil";
+
 import Title from "./common/Title";
 
-const counterState = atomFamily({
-    key: 'counterState',
-    default: param=>param,
-});
-
 const myNumberState = atom({
-    key: 'myNumberState',
-    default: 2,
-})
+  key: "myNumberState",
+  default: 2,
+});
 
 const myMultipliedState = selectorFamily({
-    key: 'MyMultipliedNumber',
-    get: (multiplier) => ({get}) => get(myNumberState) * multiplier,
+  key: "MyMultipliedNumber",
+  get: (multiplier) => ({ get }) => get(myNumberState) * multiplier,
 });
 
-const MultipliedState = ({multiplier}) => {
-    const multipliedNumber = useRecoilValue(myMultipliedState(multiplier));
-    return <p>Multiplied by <b>{multiplier}</b> = {multipliedNumber}</p>;
-}
+const MultipliedState = ({ multiplier }) => {
+  const multipliedNumber = useRecoilValue(myMultipliedState(multiplier));
+  return (
+    <p>
+      Multiplied by <b>{multiplier}</b> = {multipliedNumber}
+    </p>
+  );
+};
+
 const Number = () => {
-    const number = useRecoilValue(myNumberState);
-  return <h3>Number: {number}</h3>
-}
+  const number = useRecoilValue(myNumberState);
+  return <h3>Number: {number}</h3>;
+};
 
 export default () => (
-    <>
-        <Title>Sample For: selectorFamily</Title>
-        <Number/>
-        <MultipliedState multiplier={5}/>
-        <MultipliedState multiplier={6}/>
-    </>
+  <>
+    <Title>Sample For: selectorFamily</Title>
+    <Number />
+    <MultipliedState multiplier={5} />
+    <MultipliedState multiplier={6} />
+  </>
 );
